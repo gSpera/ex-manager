1de23a3058ca0e77d74968b7babc85767e6b5c0f
Giovanni Spera <giovanni.spera2011@libero.it>
2021-06-30 21:33:36 +0200

    Core: Add flag submitter status

--- flag.go
+++ flag.go
@@ -7,6 +7,30 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+// SubmittedFlagStatus contains the response of the flag
+type SubmittedFlagStatus string
+
+const (
+	// FlagNotSubmitted is A flag not yet submitted, we now nothing about it's state
+	FlagNotSubmitted SubmittedFlagStatus = "NOT SUBMITTED"
+	// FlagSubmittedSuccessfully is a flag that has been submitted correctly
+	FlagSubmittedSuccesfully = "SUCCESS"
+	// FlagExpired is a flag that has expired
+	FlagExpired = "EXPIRED"
+	// FlagInvalid is an invalid flag
+	FlagInvalid = "INVALID"
+	// FlagAlreadySubmitted is a flag that has alredy been submitted by the submitter
+	FlagAlredySubmitted = "ALREDY-SUBMITTED"
+	// Flagown is a flag that was obtained by the submitter team
+	FlagOwn = "TEAM-OWN"
+	// FlagNOP is a flag obtained by the NOP team
+	FlagNOP = "TEAM-NOP"
+	// FlagOffline is a flag that has been submitted when the ctf was closed
+	FlagOffline = "OFFLINE-CTF"
+	// FlaggServiceOffline is a flag that has been submitted when the service was offline
+	FlagServiceOffline = "OFFLINE-SERVICE"
+)
+
 // FlagRetriveWriter creates a io.Writer, when wrote the content is logged and flags are searched
 func FlagRetriveWriter(l *log.Entry, t Target, e *Exploit) io.Writer {
 	pr, pw := io.Pipe()
